955c847b630da7327c2674c9ab25e15470bb2122
Astuti Kumari <ka5566498@gmail.com>
2026-04-01 20:10:08 +0530

    update

--- fronted/script.js
+++ fronted/script.js
@@ -1,48 +1,44 @@
-document.querySelector("button").addEventListener("click", async () => {
+document.getElementById("analyzeBtn").addEventListener("click", async () => {
 
-    let input = document.querySelector("input").value;
+    let input = document.getElementById("inputData").value;
     let data = input.split(",").map(Number);
 
     if(data.length !== 4){
-    alert("⚠️ Please enter exactly 4 values (posts, followers, following, activity)");
-    return;
-}
+        alert("Enter 4 values");
+        return;
+    }
 
-    let response = await fetch("https://anomalous-user-detection-gnn.onrender.com/predict", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            data: data
-        })
-    });
-
-    let result = await response.json();
-
-    let popup = document.getElementById("popup");
-    let text = document.getElementById("popupResult");
-    let msg = document.getElementById("popupMsg");
-
-    popup.style.display = "flex";
-
-    if(result.result === "Anomalous"){
-        text.innerText = "🚨 Result: Anomalous";
-        text.style.color = "red";
-        msg.innerText = "⚠️ Suspicious behavior detected!";
-    } else {
-        text.innerText = "🎉 Result: Normal";
-        text.style.color = "lightgreen";
-        msg.innerText = "✅ Congrats! User is safe.";
-
-        confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 }
+    try {
+        let response = await fetch("https://anomalous-user-detection-gnn.onrender.com/predict", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                data: data
+            })
         });
+
+        let result = await response.json();
+
+        let popup = document.getElementById("popup");
+        let text = document.getElementById("popupResult");
+        let msg = document.getElementById("popupMsg");
+
+        popup.style.display = "flex";
+
+        if(result.result === "Anomalous"){
+            text.innerText = "🚨 Result: Anomalous";
+            text.style.color = "red";
+            msg.innerText = "⚠️ Suspicious behavior detected!";
+        } else {
+            text.innerText = "🎉 Result: Normal";
+            text.style.color = "lightgreen";
+            msg.innerText = "✅ User is safe.";
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Backend not responding");
     }
 });
-
-function closePopup(){
-    document.getElementById("popup").style.display = "none";
-}
